97e58d8c57b1783ff8a4b3dbb7ef220261f98c7a
Mariia Randarevych <renij1986@gmail.com>
2021-03-30 14:12:27 +0300

    добавлено удаление, фильтрация и добавление контакта с помощью redux

--- src/App.js
+++ src/App.js
@@ -4,65 +4,72 @@ import "./App.css";
 import ContactForm from "./components/ContactForm";
 import Filter from "./components/Filter";
 import ContactList from "./components/ContactList";
+import {connect} from "react-redux"
+import userActions from "./redux/actions/userActions"
+
+
 
 class App extends Component {
-  state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+  // state = {
+  //   contacts: [
+  //     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  //     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  //     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  //     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  //   ],
 
-    filter: "",
-  };
+  //   filter: "",
+  // };
   
-   componentDidMount(){
-const renewContacts=JSON.parse(localStorage.getItem("contacts"))
-if(renewContacts){
-  this.setState(()=>({ contacts:[...renewContacts]}))
-}
+//    componentDidMount(){
+// const renewContacts=JSON.parse(localStorage.getItem("contacts"))
+// if(renewContacts){
+//   this.setState(()=>({ contacts:[...renewContacts]}))
+// }
 
-   }
+//    }
 
- componentDidUpdate(prevProps, prevState){
-   const{contacts}=this.state
+//  componentDidUpdate(prevProps, prevState){
+//    const{contacts}=this.props
 
-if(contacts!==prevState.contacts){
-  localStorage.setItem("contacts", JSON.stringify(contacts))
-}
+// if(contacts!==prevState.contacts){
+//   localStorage.setItem("contacts", JSON.stringify(contacts))
+// }
 
- }
+//  }
 
 
   onHandleSubmit = (newContact) => {
-    console.log(newContact);
+    // console.log(newContact);
+    
     if (
-      this.state.contacts.some(
+      this.props.contacts.some(
         el=> el.name.toLowerCase() === newContact.name.toLowerCase()
       )
     ) {
       alert(`${newContact.name} is already in contacts`);
       return;
     }
-
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    this.props.addContact(newContact)
+    // this.setState((prevState) => ({
+    //   contacts: [...prevState.contacts, newContact],
+    // }));
+     
   };
 
   onClickDelete = (id) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((el) => el.id !== id),
-    }));
+    // this.setState((prevState) => ({
+    //   contacts: prevState.contacts.filter((el) => el.id !== id),
+    // }));
+    this.props.deleteCont(id)
   };
 
   handleFilter = (e) => {
-    this.setState({ filter: e.currentTarget.value });
+    this.props.filterAct(e.currentTarget.value )
   };
 
   render() {
-    const { contacts, filter } = this.state;
+    const { contacts, filter } = this.props;
     const normalaizedFilter = filter.toLowerCase();
     const filteredNames = contacts.filter((el) =>
       el.name.toLowerCase().includes(normalaizedFilter)
@@ -84,5 +91,14 @@ if(contacts!==prevState.contacts){
     );
   }
 }
+const mapStateToProps=(state)=>({
+contacts:state.contacts.items,
+filter:state.contacts.filter
+})
+const mapDispatchToProps={
+addContact:userActions.addContact,
+deleteCont:userActions.deleteContact,
+filterAct:userActions.filterContacts
+}
 
-export default App;
+export default  connect(mapStateToProps,mapDispatchToProps)(App);
